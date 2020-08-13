d4f4a2c0eca260df73c4da586e36849ab1424a70
Alexander Czigler <alexander@iteam.se>
2020-08-13 14:16:36 +0200

    chore: cleaning up inb4 dev

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,49 +1,23 @@
 <script>
   import page from 'page'
 
-  import Hem from './Hem.svelte'
-  import Medarbetare from './Medarbetare.svelte'
+  import Hem from './pages/Hem.svelte'
+  import Medarbetare from './pages/Medarbetare.svelte'
 
   /**
    * Routing
    */
-  let activeArea = 'hem'
-  const viewQueryStringParameter = 'show'
-  const show = area => {
-    activeArea = area
-    console.log('set area', activeArea)
-  }
-
-  const urlParams = new URLSearchParams(window.location.search)
-  const activeView = urlParams.get(viewQueryStringParameter)
-  if (!!activeView) {
-    show(activeView)
-  }
-
-  // set default component
   let current = Hem
-
-  // Map routes to page. If a route is hit the current
-  // reference is set to the route's component
   page('/', () => (current = Hem))
   page('/medarbetare', () => (current = Medarbetare))
-  // activate router
   page.start()
 </script>
 
 <style>
   main {
-    text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
@@ -65,5 +39,4 @@
   </ul>
 
   <svelte:component this={current} />
-
 </main>
--- web/src/config/ghost_key.js
+++ web/src/config/ghost_key.js
@@ -1,2 +1,2 @@
 // NOTE: This file is overwritten during the docker build step.
-export const GHOST_KEY = 'cc3275272001d3f634bfc8c632'
+export const GHOST_KEY = 'e1b252686ec60b429aa17d742f'
