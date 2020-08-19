b18b4a4172d5d146537e106f9c2d6056e518db84
Alexander Czigler <alexander@iteam.se>
2020-08-19 14:50:44 +0200

    feat: footer content

--- sapper/src/components/Footer.svelte
+++ sapper/src/components/Footer.svelte
@@ -1,7 +1,33 @@
 <div class="bg-black px-20">
-  <p class="text-white py-20">
+  <p class="text-white py-20 max-w-2xl">
     Vi är en digitaliseringsbyrå med kontor i Stockholm och Göteborg. Vi är 25
     personer som drivs av tre grundläggande värderingar – att göra något bra,
     att ha kul och att skapa värde.
   </p>
+  <div class="flex">
+    <div>
+      <p class="text-white font-medium tracking-wide">Iteam Stockholm</p>
+      <p class="text-white font-light text-sm leading-7">
+        Östermalmsgatan 26A
+        <br />
+        114 26 Stockholm
+        <br />
+        <a href="maps.google.com">Google Maps</a>
+      </p>
+    </div>
+    <div class="px-20">
+      <p class="text-white font-medium tracking-wide">Iteam Göteborg</p>
+      <p class="text-white font-light text-sm leading-7">
+        Järntorgsgatan 12-14
+        <br />
+        413 01 Göteborg
+        <br />
+        <a href="maps.google.com">Google Maps</a>
+      </p>
+    </div>
+
+  </div>
+  <div class="py-10">
+    <p class="text-white text-right">Facebook Instagram LinkedIn Youtube</p>
+  </div>
 </div>
