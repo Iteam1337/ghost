562ebced1d84fb94b9d7ea19837fa53edd886e8b
Alexander Czigler <alexander@iteam.se>
2020-08-19 14:11:45 +0200

    feat: content

--- sapper/src/routes/index.svelte
+++ sapper/src/routes/index.svelte
@@ -33,12 +33,12 @@
 <div class="lg:container lg:mx-auto py-20">
   <div class="flex px-20 max-w-3xl">
     <div class="flex-initial">
-      <h1>Kod, kultur och strategi</h1>
-      <p>
+      <h1 class="text-4xl font-medium">Kod, kultur och strategi</h1>
+      <p class="font-light text-sm">
         Genom agilitet, användarfokus och i nära samarbete med våra kunder
         skapar vi digital innovation.
       </p>
-      <p>
+      <p class="font-medium text-sm">
         <a href="/about">Så här går det till &rightarrow;</a>
       </p>
     </div>
@@ -116,4 +116,52 @@
   <a class="bordered" href="/services">Alla tjänster</a>
 </div>
 
-<h2>Vi är Iteam</h2>
+<div class="lg:container lg:mx-auto py-20 px-20">
+  <div class="flex justify-around max-w-3xl">
+    <div class="">
+      <h1 class="text-4xl font-medium">Vi är Iteam</h1>
+      <p class="font-light text-base">
+        Vi är en digital byrå med teknik som spetskompetens. Vi är ett tätt
+        sammansvetsat gäng som jobbar med moderna tekniker och metoder. Vi
+        brinner för att hjälpa våra kunder att navigera i det digitala
+        landskapet och dra nytta av digitaliseringens möjligheter.
+      </p>
+    </div>
+    <div class="px-10">
+      <img class="" src="Commodore-Joystick-Lowres.jpg" alt="commodore" />
+    </div>
+  </div>
+</div>
+
+<div class="lg:container lg:mx-auto py-20 px-20">
+  <div class="flex justify-around max-w-3xl">
+    <div class="px-10">
+      <img class="" src="Neon-Sign-Lowres.jpg" alt="commodore" />
+    </div>
+    <div class="">
+      <h1 class="text-4xl font-medium">Vi löser komplexa utmaningar</h1>
+      <p class="font-light text-base">
+        Vi kombinerar strategi, kod och kultur för att ta fram digitala
+        lösningar som möter användarnas behov. Vi utvecklar inte bara appar. Vi
+        tar ett helhetligt perspektiv på din affär för att skapa förändring på
+        riktigt. Vi leder förändringsarbetet och hjälper er att digitalisera er
+        affär.
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="lg:container lg:mx-auto py-20 px-20">
+  <div class="flex justify-around max-w-3xl">
+    <div class="">
+      <h1 class="text-4xl font-medium">Er digitala partner</h1>
+      <p class="font-light text-base">
+        Med ett långsiktigt engagemang hjälper vi er att navigera i den digitala
+        världen. Vi omvandlar visioner till verklighet, utmanar föråldrade
+        metoder och tillsammans utvecklar vi digitala tjänster som era kunder
+        älskar att använda.
+      </p>
+    </div>
+    <div class="px-10" />
+  </div>
+</div>
