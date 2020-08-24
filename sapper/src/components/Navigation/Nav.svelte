e55de740f103030f304bf58cfe3c32dd4c2f8da4
Alexander Czigler <alexander@iteam.se>
2020-08-24 15:02:38 +0200

    feat: responsive menu

--- sapper/src/components/Navigation/Nav.svelte
+++ sapper/src/components/Navigation/Nav.svelte
@@ -1,21 +1,35 @@
 <script>
   import NavItem from './NavItem.svelte'
+  import Hamburger from '../../assets/hamburger.svg'
+
   export let segment
+
+  let menuHidden = true
+
+  const toggleMenu = () => {
+    menuHidden = !menuHidden
+  }
 </script>
 
-<nav class="lg:container lg:mx-auto flex justify-between">
+<nav class="sm:container flex justify-between mx-5">
   <a
     class="py-5"
     aria-current={segment === undefined ? 'page' : undefined}
     href=".">
     <img src="iteam.png" alt="Iteam" class="logo" style="width: 100px;" />
   </a>
-  <ul class="p-0 m-0">
-    <NavItem text="Case" to="case" {segment} />
-    <NavItem text="Tjänster" to="services" {segment} />
-    <NavItem text="Om oss" to="about" {segment} />
-    <NavItem text="Karriär" to="career" {segment} />
-    <NavItem text="Kontakt" to="contact" {segment} />
-    <NavItem text="Blogg" to="blog" {segment} />
+
+  <ul class="p-0 m-0 flex flex-col items-end sm:block">
+    <button
+      on:click={() => toggleMenu()}
+      class="sm:hidden my-5 mx-0 px-0 h-6 w-6 fill-current">
+      <Hamburger />
+    </button>
+    <NavItem hidden={menuHidden} text="Case" to="case" {segment} />
+    <NavItem hidden={menuHidden} text="Tjänster" to="services" {segment} />
+    <NavItem hidden={menuHidden} text="Om oss" to="about" {segment} />
+    <NavItem hidden={menuHidden} text="Karriär" to="career" {segment} />
+    <NavItem hidden={menuHidden} text="Kontakt" to="contact" {segment} />
+    <NavItem hidden={menuHidden} text="Blogg" to="blog" {segment} />
   </ul>
 </nav>
--- sapper/src/components/Navigation/NavItem.svelte
+++ sapper/src/components/Navigation/NavItem.svelte
@@ -1,7 +1,9 @@
 <script>
-  export let segment, to, text
+  export let segment, to, text, hidden
 </script>
 
-<li class="text-black px-2 py-4 font-medium block float-left pl-6">
+<li
+  class="text-black px-2 py-4 font-medium block float-left pl-6 hidden sm:block"
+  class:hidden>
   <a aria-current={segment === to ? 'page' : undefined} href={to}>{text}</a>
 </li>
