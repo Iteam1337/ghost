c454fb8040e6684968d630669ee3b613e69d8e36
Alexander Czigler <alexander@iteam.se>
2020-08-27 09:45:19 +0200

    feat: mobile adaptations

--- sapper/src/components/ContactBanner.svelte
+++ sapper/src/components/ContactBanner.svelte
@@ -1,5 +1,7 @@
-<div class="bg-grey-medium py-32 flex flex-col items-center">
-  <h1 class="text-white pb-8 px-20 md:px-0">
+<div
+  class="bg-grey-medium py-10 sm:py-16 md:py-22 lg:py-28 xl:py-32 flex flex-col
+  items-center">
+  <h1 class="text-white text-center pb-8 px-20 md:px-0">
     Nyfiken på vad vi kan göra tillsammans?
   </h1>
   <a class="bg-cornflower text-white py-2 px-10 rounded-full" href="/contact">
