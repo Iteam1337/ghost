3832c1ca7f8256fb460d28ebfce1358e0e01bc4b
Alexander Czigler <alexander@iteam.se>
2020-08-27 11:53:53 +0200

    feat: responsive coworker list

--- sapper/src/routes/about/coworkers.svelte
+++ sapper/src/routes/about/coworkers.svelte
@@ -9,6 +9,8 @@
 <script>
   export let pages
 
+  import helpers from '../../utils/helpers'
+
   import Typography from '../../components/typography/'
   import FeaturedPosts from '../../components/FeaturedPosts.svelte'
   import Links from '../../components/links'
@@ -36,13 +38,27 @@
     </div>
 
   </div>
-  <div class="px-8 md:px-16 lg:px-32 pt-32">
-    {#each pages as page}
-      <img src={page.feature_image} alt={page.title} />
+
+  <div class="flex flex-wrap justify-center px-8 md:px-16 lg:px-32 pt-32">
+    {#each pages as coworker}
+      <div
+        class="transform hover:shadow-hover transition duration-100 ease-in
+        shadow-featured bg-white p-4 rounded-md my-2 mx-2 w-full sm:w-2/5
+        xl:w-1/5">
+        <a rel="prefetch" href={`/about/coworker/${coworker.slug}`}>
+          <div class="h-full pb-4">
+            <div class="relative" style="height: 90%;">
+              <img
+                class="rounded-md object-cover }"
+                src={coworker.feature_image}
+                alt="feature" />
+              <p class="mt-4 font-medium text-center">{coworker.title}</p>
+              <p class="font-light text-center">{coworker.excerpt}</p>
+            </div>
+          </div>
+        </a>
+
+      </div>
     {/each}
   </div>
-
-  <div
-    class="flex flex-wrap justify-center my-20 px-20 items-center md:container
-    md:mx-auto space-x-3" />
 </div>
