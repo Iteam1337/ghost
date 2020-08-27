349ab3ae7ba3a94984a05f0e3119fdba6871fc1a
Alexander Czigler <alexander@iteam.se>
2020-08-27 09:59:20 +0200

    feat: mobile adaptations

--- sapper/src/components/ContactBanner.svelte
+++ sapper/src/components/ContactBanner.svelte
@@ -1,6 +1,4 @@
-<div
-  class="bg-grey-medium py-10 sm:py-16 md:py-22 lg:py-28 xl:py-32 flex flex-col
-  items-center">
+<div class="bg-grey-medium py-10 sm:py-16 xl:py-32 flex flex-col items-center">
   <h1 class="text-white text-center pb-8 px-20 md:px-0">
     Nyfiken på vad vi kan göra tillsammans?
   </h1>
