8d256f63d0b494dfd244614e262e0690b4497617
izabellalarsson <izzwii.izabella@gmail.com>
2020-09-17 10:17:57 +0200

    chore: include custom link color

--- sapper/src/components/links/ArrowTextLink.svelte
+++ sapper/src/components/links/ArrowTextLink.svelte
@@ -1,8 +1,9 @@
 <script>
   export let to
+  export let linkColor
 </script>
 
-<p class="font-medium text-sm text-cornflower">
+<p class={`font-medium text-sm ${linkColor ? linkColor : 'text-cornflower'}`}>
   <a href={to}>
     <slot />
     &rightarrow;
