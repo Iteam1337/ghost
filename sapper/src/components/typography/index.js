713b1a6342264307f95db87a5ff6c5e6864e32fa
izabellalarsson <izzwii.izabella@gmail.com>
2020-09-30 13:50:06 +0200

    feat: add paragraph component

--- sapper/src/components/typography/index.js
+++ sapper/src/components/typography/index.js
@@ -1,5 +1,6 @@
 import BaseParagraph from './BaseParagraph.svelte'
 import H1 from './H1.svelte'
 import H4 from './H4.svelte'
+import Paragraph from './Paragraph.svelte'
 
-export default { BaseParagraph, H1, H4 }
+export default { BaseParagraph, H1, H4, Paragraph }
