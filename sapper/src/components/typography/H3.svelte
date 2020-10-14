c4a401687bebfc27061153a6e52e5ccdeba03dd7
Alexander Czigler <alexander@iteam.se>
2020-10-14 16:54:16 +0200

    feat: Add H3

---
+++ sapper/src/components/typography/H3.svelte
@@ -0,0 +1,4 @@
+<h4
+  class="m-heading font-medium sm:text-lg lg:text-xl leading-tight tracking-wide mb-0">
+  <slot />
+</h4>
--- sapper/src/components/typography/index.js
+++ sapper/src/components/typography/index.js
@@ -2,6 +2,7 @@ import ParagraphSm from './ParagraphSm.svelte'
 import ParagraphMd from './ParagraphMd.svelte'
 import ParagraphLg from './ParagraphLg.svelte'
 import H1 from './H1.svelte'
+import H3 from './H3.svelte'
 import H4 from './H4.svelte'
 
-export default { ParagraphSm, H1, H4, ParagraphMd, ParagraphLg }
+export default { ParagraphSm, H1, H3, H4, ParagraphMd, ParagraphLg }
