7ad9bf5f9a0375d01b488628a0e3f935f838e8a9
momentiris <momentiris@gmail.com>
2020-10-22 16:23:11 +0200

    chore: align blog post content

--- sapper/src/components/layout/Post.svelte
+++ sapper/src/components/layout/Post.svelte
@@ -2,6 +2,7 @@
   export let type
 </script>
 
-<div class="post {type} max-w-screen-lg px-8 mx-auto pb-10 sm:pb-12 md:pb-16 lg:pb-20 xl:pb-24">
+<div
+  class="post {type} max-w-screen-lg px-8 lg:px-0 mx-auto pb-10 sm:pb-12 md:pb-16 lg:pb-20 xl:pb-24">
   <slot />
 </div>
