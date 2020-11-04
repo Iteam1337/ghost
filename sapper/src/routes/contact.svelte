ca3f092232223d540934eca572cb03b307745288
Simon Lundell <lundell3@gmail.com>
2020-11-04 09:39:10 +0100

    fix: bold links

--- sapper/src/routes/contact.svelte
+++ sapper/src/routes/contact.svelte
@@ -1,8 +1,6 @@
 <script>
   import Layout from '../components/layout'
-  import Content from '../components/layout/Content.svelte'
   import Typography from '../components/typography/'
-  import ParagraphMd from '../components/typography/ParagraphMd.svelte'
   import Animation from '../components/animation'
 </script>
 
@@ -24,43 +22,49 @@
           </Typography.ParagraphMd>
         </div>
         <div class="md:px-24 py-12">
-          <div class="text-blue-cornflower">
-            <Typography.H2>Nya samarbeten</Typography.H2>
-          </div>
-          <Typography.ParagraphSm>
-            Det bästa sättet för att komma i kontakt med oss är att mejla vår
-            försäljningsansvarig Jonna på
-            <a
-              href="mailto:jonna.hjern@iteam.se"
-              class="underline">jonna.hjern@iteam.se</a>. Vi svarar vanligtvis
-            inom en dag. Du kan även ringa till henne på
-            <a href="callto:+46 72 975 53 66" class="underline">+46 72 975 53
-              66.</a>
-          </Typography.ParagraphSm>
           <div>
+            <div class="text-blue-cornflower">
+              <Typography.H2>Nya samarbeten</Typography.H2>
+            </div>
+            <Typography.ParagraphSm>
+              Det bästa sättet för att komma i kontakt med oss är att mejla vår
+              försäljningsansvarig Jonna på
+              <b><a
+                  href="mailto:jonna.hjern@iteam.se"
+                  class="underline">jonna.hjern@iteam.se</a></b>. Vi svarar
+              vanligtvis inom en dag. Du kan även ringa till henne på
+              <b><a href="callto:+46 72 975 53 66" class="underline">+46 72 975
+                  53 66.</a></b>
+            </Typography.ParagraphSm>
+          </div>
+          <div class="mt-8 md:mt-12">
             <div class="text-blue-cornflower">
               <Typography.H2>Jobba hos oss</Typography.H2>
             </div>
             <Typography.ParagraphSm>
               Är du intresserad av att jobba hos oss är du välkommen att skicka
               din spontantansökan till
-              <a
-                href="mailto:joinus@iteam.se"
-                class="underline">joinus@iteam.se</a>.
+              <b><a
+                  href="mailto:joinus@iteam.se"
+                  class="underline">joinus@iteam.se</a></b>.
             </Typography.ParagraphSm>
           </div>
-          <div>
+          <div class="mt-8 md:mt-12">
             <div class="text-blue-cornflower">
               <Typography.H2>Övriga ärenden</Typography.H2>
             </div>
             <Typography.ParagraphSm>
               Har du övriga frågor om lokal, ekonomi eller juridik kan du
               kontakta Hans Rollman på
-              <a href="callto:‭+46 73 813 37 87" class="underline">‭+46 73 813
-                37 87</a>‬ eller
-              <a
-                href="mailto:hans.rollman@iteam.se"
-                class="underline">hans.rollman@iteam.se</a>.
+              <b>
+                <a href="callto:‭+46 73 813 37 87" class="underline">‭+46 73 813
+                  37 87</a>
+              </b>‬ eller
+              <b>
+                <a
+                  href="mailto:hans.rollman@iteam.se"
+                  class="underline">hans.rollman@iteam.se</a>
+              </b>.
             </Typography.ParagraphSm>
           </div>
         </div>
