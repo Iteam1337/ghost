10e2cd9ceebe101adddc313a8690ab3fe5827960
Jimmy Jardland <Jimjardland@users.noreply.github.com>
2020-11-11 10:55:42 +0100

    feature: 404 (#105)
    * feature: 404 with a shiba
    
    * feat: gif cover nav
    
    Co-authored-by: Simon Lundell <lundell3@gmail.com>

--- sapper/src/routes/_error.svelte
+++ sapper/src/routes/_error.svelte
@@ -1,41 +1,37 @@
 <script>
-  export let status;
-  export let error;
+  import Typography from '../components/typography'
+  import Animation from '../components/animation'
+  import Buttons from '../components/buttons'
 
-  const dev = process.env.NODE_ENV === "development";
+  export let status
+  export let error
+
+  const dev = process.env.NODE_ENV === 'development'
 </script>
-
-<style>
-  h1,
-  p {
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
-</style>
 
 <svelte:head>
   <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<div
+  class="text-center justify-center text-white items-center flex flex-col h-screen z-40">
+  <div
+    class="h-full w-full bg-cover bg-center bg-no-repeat absolute top-0 darkness"
+    style="background-image: url(floating-shiba.gif)" />
+  <div class="relative">
+    <div class="pb-6">
+      <Typography.H1>{status}</Typography.H1>
+      <Typography.ParagraphMd>{error.message}</Typography.ParagraphMd>
+    </div>
 
-<p>{error.message}</p>
+    <Animation.ButtonHover>
+      <a href="/">
+        <Buttons.Default>Till startsidan</Buttons.Default>
+      </a>
+    </Animation.ButtonHover>
+  </div>
 
-{#if dev && error.stack}
-  <pre>{error.stack}</pre>
-{/if}
+  {#if dev && error.stack}
+    <pre>{error.stack}</pre>
+  {/if}
+</div>
