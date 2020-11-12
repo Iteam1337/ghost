9b6241b8ff0909f7542234beac8391bc9058e9f4
momentiris <momentiris@gmail.com>
2020-11-12 11:52:34 +0100

    fix up contact page a bit and increase margin on index page header image to intro text

--- sapper/src/routes/contact.svelte
+++ sapper/src/routes/contact.svelte
@@ -15,45 +15,44 @@
       <div class="w-full md:px-24">
         <Typography.H1>Behöver du hjälp med en digital produkt?</Typography.H1>
         <div class="max-w-4xl">
-          <Typography.ParagraphMd>
+          <Typography.ParagraphLg>
             Vi jobbar med kunder i alla storlekar, från startups till stora
             företag. Låt oss prata om ditt projekt och hur vi kan hjälpa till
             att skapa värde.
-          </Typography.ParagraphMd>
+          </Typography.ParagraphLg>
         </div>
         <div class="md:px-24 py-12 max-w-2xl">
-          <div>
-            <div class="text-blue-cornflower">
-              <Typography.H2>Nya samarbeten</Typography.H2>
-            </div>
-            <Typography.ParagraphSm>
-              Det bästa sättet för att komma i kontakt med oss är att mejla vår
-              försäljningsansvarig Jonna på
-              <b><a
-                  href="mailto:jonna.hjern@iteam.se"
-                  class="underline">jonna.hjern@iteam.se</a></b>. Vi svarar
-              vanligtvis inom en dag. Du kan även ringa till henne på
-              <b><a href="callto:+46 72 975 53 66" class="underline">+46 72 975
-                  53 66.</a></b>
-            </Typography.ParagraphSm>
+          <div class="text-blue-cornflower">
+            <Typography.H2>Nya samarbeten</Typography.H2>
           </div>
+          <Typography.ParagraphMd>
+            Det bästa sättet för att komma i kontakt med oss är att mejla vår
+            försäljningsansvarig Jonna på
+            <b><a
+                href="mailto:jonna.hjern@iteam.se"
+                class="underline">jonna.hjern@iteam.se</a></b>. Vi svarar
+            vanligtvis inom en dag. Du kan även ringa till henne på
+            <b><a href="callto:+46 72 975 53 66" class="underline">+46 72 975 53
+                66.</a></b>
+          </Typography.ParagraphMd>
+
           <div class="mt-8 md:mt-12">
             <div class="text-blue-cornflower">
               <Typography.H2>Jobba hos oss</Typography.H2>
             </div>
-            <Typography.ParagraphSm>
+            <Typography.ParagraphMd>
               Är du intresserad av att jobba hos oss är du välkommen att skicka
               din spontantansökan till
               <b><a
                   href="mailto:joinus@iteam.se"
                   class="underline">joinus@iteam.se</a></b>.
-            </Typography.ParagraphSm>
+            </Typography.ParagraphMd>
           </div>
           <div class="mt-8 md:mt-12">
             <div class="text-blue-cornflower">
               <Typography.H2>Övriga ärenden</Typography.H2>
             </div>
-            <Typography.ParagraphSm>
+            <Typography.ParagraphMd>
               Har du övriga frågor om lokal, ekonomi eller juridik kan du
               kontakta Hans Rollman på
               <b>
@@ -65,7 +64,7 @@
                   href="mailto:hans.rollman@iteam.se"
                   class="underline">hans.rollman@iteam.se</a>
               </b>.
-            </Typography.ParagraphSm>
+            </Typography.ParagraphMd>
           </div>
         </div>
       </div>
