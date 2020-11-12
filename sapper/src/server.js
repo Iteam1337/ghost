47afefb156ced4a724c4d18ef61ced42cc6b8e34
Simon Lundell <lundell3@gmail.com>
2020-11-12 14:23:45 +0100

    feat: redirects

--- sapper/src/server.js
+++ sapper/src/server.js
@@ -8,7 +8,54 @@ import * as sapper from '@sapper/server'
 const { PORT, NODE_ENV, GHOST_URL, GHOST_KEY } = process.env
 const dev = NODE_ENV === 'development'
 
+const redirect = (req, res, next) => {
+  const path = req.path.toLowerCase()
+
+  const redirects = {
+    '/om-oss': {
+      status: 301,
+      location: '/about',
+    },
+    '/medarbetare': {
+      status: 410,
+    },
+  }
+
+  if (Object.keys(redirects).includes(path)) {
+    res.redirect(redirects[path].status, redirects[path].location)
+  }
+
+  return next()
+}
+
+const resContent = (status) => {
+  switch (status) {
+    case 301:
+      return `Redirecting to ${location}`
+    case 410:
+      return 'Gone'
+    default:
+      return ''
+  }
+}
+
+const redirectHelper = (_, res, next) => {
+  res.redirect = (status, location) => {
+    const str = resContent(status)
+
+    res.writeHead(status, {
+      'Content-Type': 'text/plain',
+      'Content-Length': str.length,
+      ...(location && { Location: location }),
+    })
+    res.end(str)
+  }
+  return next()
+}
+
 polka()
+  .use(redirectHelper)
+  .get('/*', redirect)
   .use(
     compression({ threshold: 0 }),
     sirv('static', { dev }),
