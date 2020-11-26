19171662f22bcd863ba554e33c7d9da581af0d39
momentiris <momentiris@gmail.com>
2020-11-26 21:46:51 +0100

    improvement: remove block class from button

--- sapper/src/components/contact/Challenge.svelte
+++ sapper/src/components/contact/Challenge.svelte
@@ -7,7 +7,9 @@
   title="Har du en liknande utmaning?"
   text="Vi är redo att hjälpa era att ta det första steget mot att förverkliga er digitala vision. Kontakta oss så berättar vi mer.">
   <div class="self-start flex flex-col flex-shrink-0 md:pt-8">
-    <div class="w-56 flex-1"><img src="sales.png" alt="sales person" /></div>
+    <div class="w-56 flex-1">
+      <Image src="sales.png" alt="sales person" />
+    </div>
     <div class="mt-4">
       <p class="font-medium">Jonna Hjern</p>
       <p class="font-thin">
